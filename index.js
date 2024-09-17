8624cccb3937f8a8e6c67bf26b9d0c016fe62e1e
SosoEscola <sosoescola2024@gmail.com>
2024-09-17 11:51:58 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBPTpHTIj5iR9KFwrD9yTTbqTbJp7eWPWQ",
+  authDomain: "micro-saas-fe2d3.firebaseapp.com",
+  projectId: "micro-saas-fe2d3",
+  storageBucket: "micro-saas-fe2d3.appspot.com",
+  messagingSenderId: "74927596327",
+  appId: "1:74927596327:web:3e738ffa11682649383f02"
+};
+
+// Initialize Firebase
+export default initializeApp(firebaseConfig);
